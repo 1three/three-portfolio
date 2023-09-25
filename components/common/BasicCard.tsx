d96ceae17ba8a9e418f48a4a5d0e7dded9e43a06
1three <1_three@naver.com>
2023-09-25 20:26:46 +0900

    rename: RENAME Introduce.tsx to Info.tsx

--- components/common/BasicCard.tsx
+++ components/common/BasicCard.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-interface CardProps {
+interface BasicCardProps {
   title: string;
   contents?: string[];
 }
 
-const BasicCard: React.FC<CardProps> = ({ title, contents = [] }) => {
+// React.FC<BasicCardProps> : BasicCard 컴포넌트의 props 타입 설정
+const BasicCard: React.FC<BasicCardProps> = ({ title, contents = [] }) => {
   return (
     <div className="bg-white bg-opacity-90 p-7 rounded-3xl shadow-xl expand-on-hover">
+      {/* 카드의 제목 */}
       <h2 className="font-serif italic mb-3 text-2xl text-[#1490ab]">
         {title}
       </h2>
+
+      {/* 텍스트 내용 */}
       <div className="font-normal grid justify-between">
+        {/* contents 배열을 순회하며 각 텍스트를 p 태그로 출력 */}
         {contents.map((content, index) => (
           <p key={index} className="my-1 text-gray-800">
             {content}
--- components/common/LinkCard.tsx
+++ components/common/LinkCard.tsx
@@ -5,13 +5,18 @@ interface LinkCardProps {
   links: { name: string; url: string }[];
 }
 
+// React.FC<LinkCardProps> : LinkCard 컴포넌트의 props 타입 설정
 const LinkCard: React.FC<LinkCardProps> = ({ title, links }) => {
   return (
     <div className="bg-white bg-opacity-90 p-7 rounded-3xl shadow-xl expand-on-hover">
+      {/* 카드의 제목 */}
       <h2 className="font-serif italic mb-3 text-2xl text-[#2e99b1]">
         {title}
       </h2>
+
+      {/* 링크의 이름과 URL */}
       <div className="font-normal grid justify-between">
+        {/* links 배열을 순회하며 각 링크를 p 태그로 출력 */}
         {links.map((link, index) => (
           <p key={index} className="my-1 text-gray-800">
             {link.name}{" "}
